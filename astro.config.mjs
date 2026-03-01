28f9b430d1f1b7b5c89e369cc68cd6f6c8044c09
12asim <asim.tehmezoglu@gmail.com>
2026-02-28 22:50:10 -0500

    fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,11 @@
 import { defineConfig } from 'astro/config'
 import tailwindcss from '@tailwindcss/vite'
 
+<<<<<<< HEAD
+=======
+import tailwindcss from '@tailwindcss/vite';
+
+>>>>>>> 80bab45 (fix)
 export default defineConfig({
   site: 'https://12asim.github.io',
   devToolbar: {
